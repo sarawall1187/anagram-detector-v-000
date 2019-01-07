ac9358c04a060204af39d7ea6ddb2659e012eff6
Sara Walker <sarawall1187@gmail.com>
2019-01-07 16:31:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,10 +6,9 @@ class Anagram
   end 
   
   def match
-    possible_anagrams = @word.each_with_object do |word|
+   @word.each_with_object do |word|
       word = ["a", "b", "c"].split("").sort == ["c", "a", "b"].split("").sort
     end
-   possible_anagrams
   end 
   
 end
