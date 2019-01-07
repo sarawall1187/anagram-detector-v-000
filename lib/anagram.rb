a54937a3288328acdb2c10235a4aeaf136fbd344
Sara Walker <sarawall1187@gmail.com>
2019-01-07 16:39:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,11 @@
 class Anagram
   attr_accessor :word
-  
+   
   def intitialize(word)
     @word = word 
   end 
   
-  def match
+  def match(possible_anagrams)
    @word.each_with_object do |word|
       word = ["a", "b", "c"].split("").sort == ["c", "a", "b"].split("").sort
     end
